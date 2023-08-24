5f691cf9048642a025a2ed0e6fe22cab7a0632ca
Thái Dân <103986347+thaidan13@users.noreply.github.com>
2023-08-24 21:38:15 +0700

    ok

--- assets/js/nhan-vien.js
+++ assets/js/nhan-vien.js
@@ -1,25 +1,4 @@
-var buttons = document.getElementsByClassName('tablinks');
-var contents = document.getElementsByClassName('tabcontent');
-function showContent(id) {
-    for (var i = 0; i < contents.length; i++) {
-        contents[i].style.display = 'none';
-    }
-    var content = document.getElementById(id);
-    content.style.display = 'block';
-}
-for (var i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener("click", function () {
-        var id = this.textContent;
-        for (var i = 0; i < buttons.length; i++) {
-            buttons[i].classList.remove("active");
-        }
-        this.className += " active";
-        showContent(id);
-    });
-}
-showContent('A13');
-
-
+// Thẻ tabs
 $(document).ready(function () {
     // Hàm active tab nào đó
     function activeTab(obj) {
